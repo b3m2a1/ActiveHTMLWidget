21e55fe1b0bcebd61981de9f59962ca6f4a31cd2
b3m2a1 <b3m2a1@gmail.com>
2022-05-16 08:48:47 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 root=$PWD
 target=$1
 node_modules/.bin/sass css/bootstrap.scss css/bootstrap.css && \
+rm ActiveHTMLWidget/labextension/build_log.json && \
 python3 postprocess_css.py && \
   npm run build && \
   rm -rf $target && \
